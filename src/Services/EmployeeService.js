868a09f8806a7520bd661180360be6454ff89c7d
SurajBhalerao27 <154430492+SurajBhalerao27@users.noreply.github.com>
2024-06-13 17:45:51 +0530

    Delete src directory

--- src/Services/EmployeeService.js
+++
@@ -1,6 +0,0 @@
-import axios from 'axios';
-
-const REST_API_BASE_URL = 'http://localhost:8080/api/ems/employees';
-
-export const ListEmployees = () => axios.get(REST_API_BASE_URL);
-export const createEmployee = (employee) => axios.post(REST_API_BASE_URL, employee);
