2fee4a35db45ffca92df3a448f6f6e6fa3df3e8f
SurajBhalerao27 <154430492+SurajBhalerao27@users.noreply.github.com>
2024-06-13 17:45:09 +0530

    Add files via upload

---
+++ src/Components/ListEmployeeComponent.jsx
@@ -0,0 +1,59 @@
+import React, { useEffect } from 'react'
+import { useState } from 'react'
+import { ListEmployees } from '../Services/EmployeeService';
+import {useNavigate } from 'react-router-dom';
+
+
+const ListEmployeeComponent = () => {
+
+    const [employees, setEmployees] = useState([]);
+
+    const navigator =  useNavigate();
+
+    useEffect(() => {
+        ListEmployees()
+        .then((response) => {
+            setEmployees(response.data);
+        })
+        .catch(error => {
+            console.error(error);
+        });
+    }, [])
+
+    function addNewEmployee(){
+        navigator('/add-employee');
+    }
+
+    return (
+        <div className='container mt-5'>
+            <h2 className='text-center'>List Of Employees</h2>
+            <button className='btn btn-primary mb-3' onClick={addNewEmployee}>Add Employee</button>
+            <table className='table table-striped table-bordered'>
+                <thead className='thead-dark'>
+                    <tr>
+                        <th>Id</th>
+                        <th>First Name</th>
+                        <th>Last Name</th>
+                        <th>Email</th>
+                    </tr>
+                </thead>
+                <tbody>
+                    {
+                        employees.map(employee => {
+                            return (
+                                <tr key={employee.id}>
+                                    <td>{employee.id}</td>
+                                    <td>{employee.firstName}</td>
+                                    <td>{employee.lastName}</td>
+                                    <td>{employee.email}</td>
+                                </tr>
+                            )
+                        })
+                    }
+                </tbody>
+            </table>
+        </div>
+    )
+}
+
+export default ListEmployeeComponent 
